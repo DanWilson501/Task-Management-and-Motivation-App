65281895db590a675386e78d1918c524d3f3b13a
Daniel Wilson <danwilson501@gmail.com>
2024-07-11 18:05:28 -0400

    Tasks variable created in its module.

---
+++ task-manager-app/src/tasks.js
@@ -0,0 +1,9 @@
+const tasks = [
+    { id: 1, name: 'Healthy breakfast: eggs, bacon and veggies.', isCompleted: false },
+    { id: 2, name: 'Light morning stretch to circulate blood flow.', isCompleted: false },
+    { id: 3, name: 'Plan out future tasks, such as chores or homework.', isCompleted: true },
+    { id: 4, name: 'Practice some basketball at the gym.', isCompleted: false },
+  ];
+  
+  export default tasks;
+  
